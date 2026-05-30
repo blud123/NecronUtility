e37224724a18cdafacd619737db3a23b79e10790
deWinterr <162336755+deWinterr@users.noreply.github.com>
2026-05-30 14:36:39 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     alias(libs.plugins.fabric.loom)
 }
+
 base {
     archivesName = properties["archives_base_name"] as String
     version = libs.versions.mod.version.get()
     group = properties["maven_group"] as String
 }
+
 repositories {
     maven {
         name = "meteor-maven"
@@ -16,21 +18,25 @@ repositories {
         url = uri("https://maven.meteordev.org/snapshots")
     }
 }
+
 dependencies {
     minecraft(libs.minecraft)
     mappings(loom.officialMojangMappings())
     modImplementation(libs.fabric.loader)
     modImplementation(libs.meteor.client)
 }
+
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(libs.versions.jdk.get().toInt()))
     }
 }
+
 fun toMinecraftCompat(version: String): String {
     val parts = version.split(".")
     return "~${parts[0]}.${parts[1]}"
 }
+
 tasks {
     processResources {
         val propertyMap = mapOf(
@@ -43,16 +49,17 @@ tasks {
             expand(propertyMap)
         }
     }
+    
     jar {
         inputs.property("archivesName", project.base.archivesName.get())
         from("LICENSE") {
             rename { "${it}_${inputs.properties["archivesName"]}" }
         }
     }
+    
     withType<JavaCompile>().configureEach {
         options.compilerArgs.addAll(
             listOf(
-                "--enable-preview",
                 "-Xlint:deprecation",
                 "-Xlint:unchecked"
             )
