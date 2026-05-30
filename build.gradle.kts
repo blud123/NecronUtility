e4f600c90adb5010aaf42d42f2017a5027eb30d1
deWinterr <162336755+deWinterr@users.noreply.github.com>
2026-05-30 14:23:30 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
     alias(libs.plugins.fabric.loom)
 }
-
 base {
     archivesName = properties["archives_base_name"] as String
     version = libs.versions.mod.version.get()
     group = properties["maven_group"] as String
 }
-
 repositories {
     maven {
         name = "meteor-maven"
@@ -18,7 +16,6 @@ repositories {
         url = uri("https://maven.meteordev.org/snapshots")
     }
 }
-
 dependencies {
     // Fabric
     minecraft(libs.minecraft)
@@ -27,18 +24,15 @@ dependencies {
     // Meteor
     modImplementation(libs.meteor.client)
 }
-
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(libs.versions.jdk.get().toInt()))
     }
 }
-
 fun toMinecraftCompat(version: String): String {
     val parts = version.split(".")
     return "~${parts[0]}.${parts[1]}"
 }
-
 tasks {
     processResources {
         val propertyMap = mapOf(
@@ -46,30 +40,23 @@ tasks {
             "minecraft_version" to toMinecraftCompat(libs.versions.minecraft.get()),
             "jdk_version" to libs.versions.jdk.get(),
         )
-
         inputs.properties(propertyMap)
         filesMatching("fabric.mod.json") {
             expand(propertyMap)
         }
     }
-
     jar {
         inputs.property("archivesName", project.base.archivesName.get())
-
         from("LICENSE") {
             rename { "${it}_${inputs.properties["archivesName"]}" }
         }
     }
-
     withType<JavaCompile>().configureEach {
         options.compilerArgs.addAll(
             listOf(
                 "--enable-preview",
                 "-Xlint:deprecation",
                 "-Xlint:unchecked"
-        )
-    )
-}
             )
         )
     }
