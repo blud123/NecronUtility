36fbdf6125df57ef01ee7aac382cb161e185921b
deWinterr <162336755+deWinterr@users.noreply.github.com>
2026-05-30 14:16:48 +0100

    Remove Fabric Snapshots Maven repository
    Removed Fabric Snapshots repository configuration.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,18 +4,11 @@ pluginManagement {
             name = "Fabric"
             url = uri("https://maven.fabricmc.net/")
         }
-        maven {
-            name = "Fabric Snapshots"
-            url = uri("https://maven.fabricmc.net/")
-            mavenContent { snapshotsOnly() }
-        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
-
 rootProject.name = "addon-template"
